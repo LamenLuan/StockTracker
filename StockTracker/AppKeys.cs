80f50a10c259fbae70156736c1afd1b7143aabc9
LamenLuan <luan.leme.s35@gmail.com>
2024-03-10 10:34:17 -0300

    ♻️ refactor: Parsing properties in settings parameters

---
+++ StockTracker/AppKeys.cs
@@ -0,0 +1,23 @@
+﻿using System.Configuration;
+
+namespace StockTracker
+{
+  public static class AppKeys
+  {
+    public const string BRAPI_KEY = "Brapikey";
+    public const string COOLDOWN = "Cooldown";
+    public const string START_TIME = "StartTime";
+    public const string END_TIME = "EndTime";
+
+    public static string? AppConfigValue(this string key)
+      => ConfigurationManager.AppSettings[key];
+
+    public static TimeSpan GetTimeParam(this string key)
+    {
+      if (!TimeSpan.TryParse(key.AppConfigValue(), out var setting))
+        throw new ArgumentOutOfRangeException(nameof(key));
+
+      return setting;
+    }
+  }
+}
--- StockTracker/Program.cs
+++ StockTracker/Program.cs
@@ -2,166 +2,187 @@
 using StockTracker;
 using StockTracker.Types;
 using StockTracker.Utils.Extensions;
-using System.Configuration;
 using System.Net.NetworkInformation;
 
 internal class Program
 {
-	private static readonly HttpClient _client = new();
-	private static readonly TimeSpan StartTime = new(9, 30, 0);
-	private static readonly TimeSpan EndTime = TimeSpan.FromHours(17);
-	private static readonly TimeSpan Cooldown = TimeSpan.FromMinutes(15);
-	private static readonly List<StockTriggered> StocksTriggered = new();
-	private static List<StockTracking> StocksTracked = FileManager.ReadStockTrackings();
+  private static TimeSpan Cooldown;
+  private static TimeSpan StartTime;
+  private static TimeSpan EndTime;
+  private static List<StockTracking> StocksTracked = new();
 
-	private static readonly string? ApiKey =
-		ConfigurationManager.AppSettings["Brapikey"];
+  private static readonly HttpClient _client = new();
+  private static readonly List<StockTriggered> StocksTriggered = new();
+  private static readonly string? ApiKey = AppKeys.BRAPI_KEY.AppConfigValue();
 
-	private static void Main()
-	{
-		if (!StocksTracked.Any())
-		{
-			Notify("There was nothing to track. Program closed");
-			return;
-		}
+  private static void Main()
+  {
+    ReadSettings();
+    WaitUntilStartTime();
 
-		WaitUntilStartTime();
+    while (true)
+    {
+      if (DateTime.Now.TimeOfDay >= EndTime) break;
 
-		while (true)
-		{
-			if (DateTime.Now.TimeOfDay >= EndTime) break;
+      var connectionTries = 0;
+      var apiCommunicated = true;
 
-			var connectionTries = 0;
-			var apiCommunicated = true;
+      for (int i = 0; i < StocksTracked.Count; i++)
+      {
+        var tracked = StocksTracked[i];
+        var url = $"https://brapi.dev/api/quote/{tracked.Symbol}?token={ApiKey}";
+        string? response;
 
-			for (int i = 0; i < StocksTracked.Count; i++)
-			{
-				var tracked = StocksTracked[i];
-				var url = $"https://brapi.dev/api/quote/{tracked.Symbol}?token={ApiKey}";
-				string? response;
+        try
+        {
+          response = _client.GetStringAsync(url).Result;
+        }
+        catch (Exception)
+        {
+          if (++connectionTries == 3)
+          {
+            Notify("Failed to communicate with API, program closed");
+            return;
+          }
 
-				try
-				{
-					response = _client.GetStringAsync(url).Result;
-				}
-				catch (Exception)
-				{
-					if (++connectionTries == 3)
-					{
-						Notify("Failed to communicate with API, program closed");
-						return;
-					}
+          CheckIfApiIsCommunicating(ref apiCommunicated, tracked);
+          i--;
+          continue;
+        }
 
-					CheckIfApiIsCommunicating(ref apiCommunicated, tracked);
-					i--;
-					continue;
-				}
+        connectionTries = 0;
+        apiCommunicated = true;
+        if (string.IsNullOrEmpty(response)) continue;
 
-				connectionTries = 0;
-				apiCommunicated = true;
-				if (string.IsNullOrEmpty(response)) continue;
+        var stockResults = response.Deserialize<StocksResults>();
+        if (stockResults == null) continue;
+        if (StockTriggered(stockResults, tracked)) i--;
+      }
 
-				var stockResults = response.Deserialize<StocksResults>();
-				if (stockResults == null) continue;
-				if (StockTriggered(stockResults, tracked)) i--;
-			}
+      NotifyTriggers(StocksTriggered);
+      Thread.Sleep(Cooldown);
+      ReadStockTrackings();
+    }
+  }
 
-			NotifyTriggers(StocksTriggered);
-			Thread.Sleep(Cooldown);
-			StocksTracked = FileManager.ReadStockTrackings();
-		}
-	}
+  private static void ReadSettings()
+  {
+    ReadStockTrackings();
 
-	private static bool StockTriggered(
-			StocksResults stocksResults,
-			StockTracking tracked
-		)
-	{
-		var priceNow = stocksResults.Results.First().RegularMarketPrice;
-		var incomePercentage = ((priceNow / tracked.RegularMarketPrice) - 1) * 100;
+    try
+    {
+      Cooldown = AppKeys.COOLDOWN.GetTimeParam();
+      StartTime = AppKeys.START_TIME.GetTimeParam();
+      EndTime = AppKeys.END_TIME.GetTimeParam();
+    }
+    catch (ArgumentOutOfRangeException ex)
+    {
+      Notify($"Program failed to load the setting [{ex.ParamName}]");
+      throw;
+    }
+  }
 
-		if (PriceTriggered(tracked, incomePercentage))
-		{
-			var stockTriggered = new StockTriggered(tracked, incomePercentage);
-			StocksTriggered.Add(stockTriggered);
-			StocksTracked.Remove(tracked);
-			return true;
-		}
+  private static void ReadStockTrackings()
+  {
+    StocksTracked = FileManager.ReadStockTrackings();
 
-		return false;
-	}
+    if (!StocksTracked.Any())
+    {
+      Notify("No tracker found in data file. Program closed");
+      throw new ArgumentOutOfRangeException(nameof(StocksTracked));
+    }
+  }
 
-	private static bool PriceTriggered(StockTracking tracked, float percentage)
-	{
-		return TriggeredToBuy(tracked, -percentage)
-			|| TriggeredToSell(tracked, percentage);
-	}
+  private static bool StockTriggered(
+      StocksResults stocksResults,
+      StockTracking tracked
+    )
+  {
+    var priceNow = stocksResults.Results.First().RegularMarketPrice;
+    var incomePercentage = ((priceNow / tracked.RegularMarketPrice) - 1) * 100;
 
-	private static bool TriggeredToBuy(StockTracking tracked, float percentage)
-		=> tracked.TrackingToBuy && percentage >= tracked.TriggerPercentage;
+    if (PriceTriggered(tracked, incomePercentage))
+    {
+      var stockTriggered = new StockTriggered(tracked, incomePercentage);
+      StocksTriggered.Add(stockTriggered);
+      StocksTracked.Remove(tracked);
+      return true;
+    }
 
-	private static bool TriggeredToSell(StockTracking tracked, float percentage)
-	=> !tracked.TrackingToBuy && percentage >= tracked.TriggerPercentage;
+    return false;
+  }
 
-	private static void CheckIfApiIsCommunicating(
-			ref bool apiCommunicated, StockTracking tracked
-		)
-	{
-		if (ApiIsCommunicating())
-		{
-			if (apiCommunicated) StocksTracked.Remove(tracked);
-			apiCommunicated = true;
-		}
-		else
-		{
-			Thread.Sleep(TimeSpan.FromSeconds(2));
-			apiCommunicated = false;
-		}
-	}
+  private static bool PriceTriggered(StockTracking tracked, float percentage)
+  {
+    return TriggeredToBuy(tracked, -percentage)
+      || TriggeredToSell(tracked, percentage);
+  }
 
-	private static bool ApiIsCommunicating()
-	{
-		try
-		{
-			Ping myPing = new();
-			return myPing.Send("brapi.dev").Status == IPStatus.Success;
-		}
-		catch (Exception)
-		{
-			return false;
-		}
-	}
+  private static bool TriggeredToBuy(StockTracking tracked, float percentage)
+    => tracked.TrackingToBuy && percentage >= tracked.TriggerPercentage;
 
-	private static void WaitUntilStartTime()
-	{
-		Notify("Program initialized");
-		var timeNow = DateTime.Now.TimeOfDay;
+  private static bool TriggeredToSell(StockTracking tracked, float percentage)
+  => !tracked.TrackingToBuy && percentage >= tracked.TriggerPercentage;
 
-		if (timeNow >= StartTime)
-		{
-			if (timeNow >= EndTime)
-				Thread.Sleep(TimeSpan.FromDays(1) - timeNow + StartTime);
-		}
-		else Thread.Sleep(StartTime - timeNow);
+  private static void CheckIfApiIsCommunicating(
+      ref bool apiCommunicated, StockTracking tracked
+    )
+  {
+    if (ApiIsCommunicating())
+    {
+      if (apiCommunicated) StocksTracked.Remove(tracked);
+      apiCommunicated = true;
+    }
+    else
+    {
+      Thread.Sleep(TimeSpan.FromSeconds(2));
+      apiCommunicated = false;
+    }
+  }
 
-		Notify("Tracker started");
-	}
+  private static bool ApiIsCommunicating()
+  {
+    try
+    {
+      Ping myPing = new();
+      return myPing.Send("brapi.dev").Status == IPStatus.Success;
+    }
+    catch (Exception)
+    {
+      return false;
+    }
+  }
 
-	private static void NotifyTriggers(List<StockTriggered> stocksTriggered)
-	{
-		if (stocksTriggered.Any())
-		{
-			var triggersMessages = stocksTriggered.Select(s => s.ToString());
-			var finalMessage = string.Join("\n", triggersMessages);
-			Notify(finalMessage, "Tracker Triggered!");
-		}
-	}
+  private static void WaitUntilStartTime()
+  {
+    Notify("Program initialized");
+    var timeNow = DateTime.Now.TimeOfDay;
 
-	private static void Notify(string message, string? title = null)
-	{
-		var toast = new ToastContentBuilder();
-		if (title != null) toast.AddText(title);
-		toast.AddText(message);
-		toast.Show();
-	}
+    if (timeNow >= StartTime)
+    {
+      if (timeNow >= EndTime)
+        Thread.Sleep(TimeSpan.FromDays(1) - timeNow + StartTime);
+    }
+    else Thread.Sleep(StartTime - timeNow);
+
+    Notify("Tracker started");
+  }
+
+  private static void NotifyTriggers(List<StockTriggered> stocksTriggered)
+  {
+    if (stocksTriggered.Any())
+    {
+      var triggersMessages = stocksTriggered.Select(s => s.ToString());
+      var finalMessage = string.Join("\n", triggersMessages);
+      Notify(finalMessage, "Tracker Triggered!");
+    }
+  }
+
+  private static void Notify(string message, string? title = null)
+  {
+    var toast = new ToastContentBuilder();
+    if (title != null) toast.AddText(title);
+    toast.AddText(message);
+    toast.Show();
+  }
 }
