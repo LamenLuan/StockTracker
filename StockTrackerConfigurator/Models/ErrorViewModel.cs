320225be91c6b9f993b25c9c019a948645093123
LamenLuan <luan.leme.s35@gmail.com>
2024-04-08 20:01:28 -0300

    🎉 Creating project

---
+++ StockTrackerConfigurator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StockTrackerConfigurator.Models
+{
+  public class ErrorViewModel
+  {
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
---
+++ StockTrackerConfigurator/Program.cs
@@ -0,0 +1,34 @@
+using System.Diagnostics;
+
+const string URL = "http://localhost:5000";
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.WebHost.UseUrls(URL);
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+  app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+Process.Start(new ProcessStartInfo
+{
+  FileName = URL,
+  UseShellExecute = true
+});
+
+app.Run();
